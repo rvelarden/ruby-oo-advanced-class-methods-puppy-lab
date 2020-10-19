1d9172680d68e047a3799dc27a864cf153be4dc2
Your Username <you@example.com>
2020-10-18 22:14:27 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+
+class Dog 
+    attr_accessor :name
+    
+        @@all = []
+        def save
+             @@all << self 
+        end 
+
+        def initialize(name)
+            @name = name 
+            self.save 
+            # binding.pry 
+        end
+
+        def self.all
+            @@all 
+        end
+
+        def self.print_all
+            self.all.each do |x| 
+                 puts x.name   
+            end
+        end
+
+        def self.clear_all 
+            @@all = []
+        end
+     
+        
+end
